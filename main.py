90f81f0d46abaaa1c38b28d33621a16ed9a150d6
robert-nicola <rober.nicola@ibm.com>
2025-05-06 13:38:07 +0300

    finish chapter 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_words_count, get_character_count
+import sys
+from stats import get_words_count, get_character_count, sort_dictionary
 
 
 def get_book_text(path):
@@ -8,11 +9,22 @@ def get_book_text(path):
 
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+
+    file_path = sys.argv[1]
+    text = get_book_text(file_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
     num_words = get_words_count(text)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
     character_count = get_character_count(text)
-    print(f"{num_words} words found in the document")
-    print(character_count)
+    sorted_dictionary = sort_dictionary(character_count)
+    print("--------- Character Count -------")
+    for char, count in sorted_dictionary:
+        print(f"{char}: {count}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -5,10 +5,15 @@ def get_words_count(text):
 
 def get_character_count(text):
     text = text.lower()
-    character_dictionaries = {}
+    character_dictionary = {}
     for character in text:
-        if character in character_dictionaries:
-            character_dictionaries[character] += 1
-        else:
-            character_dictionaries[character] = 1
-    return character_dictionaries
+        if character.isalpha():
+            if character in character_dictionary:
+                character_dictionary[character] += 1
+            else:
+                character_dictionary[character] = 1
+    return character_dictionary
+
+
+def sort_dictionary(character_dictionary):
+    return sorted(character_dictionary.items(), key=lambda item: item[1], reverse=True)
